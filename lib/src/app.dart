d4e1583b5dc97805abb8a9c87216e9a3a0d7d50d
Irhas <irhasm@outlook.com>
2025-07-01 10:42:29 +0700

    refactor: change App to use ConsumerWidget for state management

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,23 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'l10n/app_localizations.dart';
+import 'provider/providers.dart';
 import 'routes.dart';
 import 'themes.dart';
 
-class App extends StatelessWidget {
+class App extends ConsumerWidget {
   const App({super.key});
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final locale = ref.watch(localeProvider).locale;
+    final themeMode = ref.watch(themeProvider).themeMode;
+
     return MaterialApp(
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
+      locale: locale,
       title: 'Simulation',
       onGenerateTitle: (context) => AppLocalizations.of(context)!.appTitle,
       theme: theme,
       darkTheme: darkTheme,
-      themeMode: ThemeMode.system,
+      themeMode: themeMode,
       initialRoute: '/',
       onGenerateRoute: onGenerateRoute,
     );
